ae3de6e17ba714955280a4142fe41ce0f92ad497
taiteb <taiteb@gmail.com>
2023-05-27 02:12:17 -0600

    additional console.log comments

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log("Hello, FullStack Academy")
+console.log("Hello, FullStack Academy");
+console.log("While you're here, I'll explain the header");
+console.log("It's an iframe embed of a p5 sketch I wrote");
+console.log("Detailed notes are in the github repo the Page is hosting from");
+console.log("But basically I pieced it together from a couple different souces");
+console.log("P.S. you can click on the header to spawn more of the traces");
